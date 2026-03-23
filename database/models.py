ded2949782058f23a90dd6c44ec0fe1071fd6207
iermolaev190-cloud <iermolaev190@gmail.com>
2026-03-23 15:00:45 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True, nullable=False, index=True)
+    telegram_id = Column(BigInteger, unique=True, nullable=False, index=True)
     username = Column(String(255), nullable=True)
     first_name = Column(String(255), nullable=True)
 
